2d03327ad21ed2c8d36c25c6ed3a522a5248bdb4
SkibdiScoundrel <cholewickialec@gmail.com>
2025-08-14 14:28:26 -0400

    Created black screen

---
+++ workspace/github/bootdev_asteroids/main.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    while 1==1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
