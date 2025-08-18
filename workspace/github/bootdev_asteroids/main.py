adf80b12a964e8a5d37419949ef7bc5066d2d87d
SkibdiScoundrel <cholewickialec@gmail.com>
2025-08-18 14:42:53 -0400

    Added game functionality

--- workspace/github/bootdev_asteroids/main.py
+++ workspace/github/bootdev_asteroids/main.py
@@ -1,15 +1,45 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
+clock = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    asteroid_field = AsteroidField()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots,updatable,drawable)
+
     while 1==1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+        for asteroid in asteroids:
+           if asteroid.collision(player):
+               sys.exit("Game Over!")
+        for asteroid in asteroids:
+           for bullet in shots:
+               if asteroid.collision(bullet):
+                   bullet.kill()
+                   asteroid.split()
 
 if __name__ == "__main__":
     main()
